820b3d3fb7b68c90ab6691cc61f3a1cf98015f42
LipLovesHisCat <liparit220900@gmail.com>
2023-12-10 02:00:40 +0300

    обновил urls.py, views.py

--- rialto/name/urls.py
+++ rialto/name/urls.py
@@ -7,4 +7,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('analytics', views.analytics, name='analytics'),
+    path('newsline', views.newsline, name='newsline'),
+    path('news', views.news, name='news'),
+    path('account', views.account, name='account'),
+    path('wallet', views.wallet, name='wallet'),
+    path('catalogue', views.catalogue, name='catalogue'),
+    path('review', views.review, name='review'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- rialto/name/views.py
+++ rialto/name/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'name/index.html')
+
+def dashboard(request):
+    return render(request, 'name/dashboard.html')
+
+def analytics(request):
+    return render(request, 'name/analytics.html')
+
+def newsline(request):
+    return render(request, 'name/newsline.html')
+
+def news(request):
+    return render(request, 'name/news.html')
+
+def account(request):
+    return render(request, 'name/account.html')
+
+def wallet(request):
+    return render(request, 'name/wallet.html')
+
+def catalogue(request):
+    return render(request, 'name/catalogue.html')
+
+def review(request):
+    return render(request, 'name/review.html')
